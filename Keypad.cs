d8f00a4120a0b3a14768a7f326890f77967fc76f
Foorack <max@foorack.com>
2021-04-19 23:11:58 +0200

    Added support for audio and multiple solutions/doors

--- Keypad.cs
+++ Keypad.cs
@@ -12,14 +12,18 @@ public class Keypad : UdonSharpBehaviour
 {
 
     private readonly string AUTHOR = "Foorack";
-    private readonly string VERSION = "3.2";
-    
+    private readonly string VERSION = "3.3";
+
     public string solution = "2580";
     public GameObject doorObject = null;
 
     public string[] allowList = new string[0];
     public string[] denyList = new string[0];
-
+    
+    public AudioSource soundDenied = null;
+    public AudioSource soundGranted = null;
+    public AudioSource soundButton = null;
+    
     // ReSharper disable once InconsistentNaming
     public string translationPasscode = "PASSCODE";
     // ReSharper disable once InconsistentNaming
@@ -33,16 +37,23 @@ public class Keypad : UdonSharpBehaviour
     public UdonBehaviour programClosed;
     public UdonBehaviour programDenied;
     public UdonBehaviour programGranted;
-    
+
     public Text internalKeypadDisplay = null;
+
+    public string[] additionalSolutions = new string[0];
+    public GameObject[] additionalDoorObjects = new GameObject[0];
+
+    public bool additionalKeySeparation = false;
 
     // Debugging
     private string _keypadId;
     private string _prefix;
-    
+
     // Keypad data storage
     private string _buffer;
-    
+    private string[] _solutions;
+    private GameObject[] _doors;
+
     #region Util Functions
     private void Log(string value)
     {
@@ -63,10 +74,10 @@ public class Keypad : UdonSharpBehaviour
         // Crash.
         // ReSharper disable once PossibleNullReferenceException
         // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
-        ((string) null).ToString();
+        ((string)null).ToString();
     }
     #endregion Util Functions
-    
+
     public void Start()
     {
         // ReSharper disable once SpecifyACultureInStringConversionExplicitly
@@ -76,7 +87,7 @@ public class Keypad : UdonSharpBehaviour
         Debug.Log(_prefix + "Starting Keypad... Made by @" + AUTHOR + ". Version " + VERSION + ".");
 
         _buffer = "";
-        
+
         if (solution == null)
         {
             LogError("Solution was null! Resetting to default value!");
@@ -111,7 +122,7 @@ public class Keypad : UdonSharpBehaviour
             LogError("Deny list was null, setting to empty list...");
             denyList = new string[0];
         }
-        
+
         if (allowList.Length > 9999)
         {
             LogError("Allow list was larger than 9999, this is most likely unintentional, resetting to 0.");
@@ -121,6 +132,51 @@ public class Keypad : UdonSharpBehaviour
         {
             LogError("Allow list was larger than 9999, this is most likely unintentional, resetting to 0.");
             denyList = new string[0];
+        }
+
+        if (additionalSolutions == null)
+        {
+            LogError("Additional Solutions list was null, setting to empty list...");
+            additionalSolutions = new string[0];
+        }
+        if (additionalDoorObjects == null)
+        {
+            LogError("Additional Doors list was null, setting to empty list...");
+            additionalDoorObjects = new GameObject[0];
+        }
+
+        if (additionalSolutions.Length > 9999)
+        {
+            LogError("Additional Solutions list was larger than 9999, this is most likely unintentional, resetting to 0.");
+            additionalSolutions = new string[0];
+        }
+        if (additionalDoorObjects.Length > 9999)
+        {
+            LogError("Additional Doors list was larger than 9999, this is most likely unintentional, resetting to 0.");
+            additionalDoorObjects = new GameObject[0];
+        }
+
+        if (additionalKeySeparation && additionalSolutions.Length != additionalDoorObjects.Length)
+        {
+            LogError("Key separation was enabled, but the number of additional solutions is not equal to the number of additional doors, " +
+                "resetting to False. Please read the documentation what this setting does or contact for help.");
+            additionalKeySeparation = false;
+        }
+        Log("Additional key separation is: " + additionalKeySeparation);
+
+        // Merge primary solution/door with additional solutions/doors.
+        // This makes coding and loops more streamlined.
+        _solutions = new string[additionalSolutions.Length + 1];
+        _doors = new GameObject[additionalDoorObjects.Length + 1];
+        _solutions[0] = solution;
+        _doors[0] = doorObject;
+        for (var i = 0; i != additionalSolutions.Length; i++)
+        {
+            _solutions[i + 1] = additionalSolutions[i];
+        }
+        for (var i = 0; i != additionalDoorObjects.Length; i++)
+        {
+            _doors[i + 1] = additionalDoorObjects[i];
         }
 
         internalKeypadDisplay.text = translationPasscode;
@@ -135,11 +191,13 @@ public class Keypad : UdonSharpBehaviour
         _buffer = "";
         internalKeypadDisplay.text = translationPasscode;
         
-        if (doorObject != gameObject)
-        {
-            doorObject.SetActive(hideDoorOnGranted);
+        foreach (var door in _doors) {
+            if (door != gameObject)
+            {
+                door.SetActive(hideDoorOnGranted);
+            }
         }
-        
+
         if (programDenied != null)
         {
             programClosed.SendCustomEvent("keypadClosed");
@@ -149,11 +207,11 @@ public class Keypad : UdonSharpBehaviour
     // ReSharper disable once InconsistentNaming
     private void OK()
     {
-        bool isOnAllow = false;
-        bool isOnDeny = false;
-        string username = Networking.LocalPlayer == null ? "UnityEditor" : Networking.LocalPlayer.displayName;
+        var isOnAllow = false;
+        var isOnDeny = false;
+        var username = Networking.LocalPlayer == null ? "UnityEditor" : Networking.LocalPlayer.displayName;
         // Check if user is on allow list
-        foreach (string entry in allowList)
+        foreach (var entry in allowList)
         {
             if (entry == username)
             {
@@ -161,25 +219,57 @@ public class Keypad : UdonSharpBehaviour
             }
         }
         // Check if user is on deny list
-        foreach (string entry in denyList)
+        foreach (var entry in denyList)
         {
             if (entry == username)
             {
                 isOnDeny = true;
             }
         }
+
+        var isCorrect = false;
+        GameObject correctDoor = null;
+        for (var i = 0; i != _solutions.Length; i++)
+        {
+            if (_solutions[i] != _buffer) continue;
+            isCorrect = true;
+            if (i < _doors.Length)
+            {
+                correctDoor = _doors[i];
+            }
+        }
         // Check if pass is correct and not on deny, or if is on allow list.
-        if ((_buffer == solution && !isOnDeny) || isOnAllow)
+        if ((isCorrect && !isOnDeny) || isOnAllow)
         {
             Log(isOnAllow ? "GRANTED through allow list!" : "Passcode GRANTED!");
             _buffer = "";
             internalKeypadDisplay.text = translationGranted;
             
-            if (doorObject != gameObject)
+            foreach (var door in _doors)
             {
-                doorObject.SetActive(!hideDoorOnGranted);
+                if (door == gameObject) continue;
+                if (additionalKeySeparation)
+                {
+                    if (door == correctDoor)
+                    {
+                        door.SetActive(!hideDoorOnGranted);                            
+                    }
+                    else
+                    {
+                        door.SetActive(hideDoorOnGranted);
+                    }
+                }
+                else
+                {
+                    door.SetActive(!hideDoorOnGranted);                        
+                }
             }
 
+            if (soundGranted != null)
+            {
+                soundGranted.Play();
+            }
+            
             if (programGranted != null)
             {
                 programGranted.SendCustomEvent("keypadGranted");
@@ -191,10 +281,16 @@ public class Keypad : UdonSharpBehaviour
             Log("Passcode DENIED!");
             _buffer = "";
             internalKeypadDisplay.text = translationDenied;
-            
-            if (doorObject != gameObject)
+
+            foreach (var door in _doors)
             {
-                doorObject.SetActive(hideDoorOnGranted);
+                if (door == gameObject) continue;
+                door.SetActive(hideDoorOnGranted);
+            }
+
+            if (soundDenied != null)
+            {
+                soundDenied.Play();
             }
             
             if (programDenied != null)
@@ -206,21 +302,22 @@ public class Keypad : UdonSharpBehaviour
 
     private void PrintPassword()
     {
-        string pass = "*";
-        for (int i = 1; i < _buffer.Length; i++)
+        var pass = "*";
+        for (var i = 1; i < _buffer.Length; i++)
         {
             pass += " *";
         }
 
         internalKeypadDisplay.text = pass;
     }
-    
+
     public void ButtonInput(string inputValue)
     {
         if (inputValue == "CLR")
         {
             CLR();
-        } else if (inputValue == "OK")
+        }
+        else if (inputValue == "OK")
         {
             OK();
         }
@@ -235,8 +332,12 @@ public class Keypad : UdonSharpBehaviour
                 _buffer += inputValue;
                 PrintPassword();
                 Log("Buffer appended: " + inputValue);
+                if (soundButton != null)
+                {
+                    soundButton.Play();
+                }
             }
         }
     }
-    
+
 }
